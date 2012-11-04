9c153638496c3eaf858e63911dc2ee01d88a5672
Kamil Gałuszka <galuszkak@gmail.com>
2012-11-04 23:34:15 +0100

    changing session store

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var MongoStore = require('connect-mongo')(express);
 
 app.configure(function(){
     var cwd = process.cwd();
@@ -10,7 +11,7 @@ app.configure(function(){
     app.set('cssDirectory', '/stylesheets/');
     app.use(express.bodyParser());
     app.use(express.cookieParser('secret'));
-    app.use(express.session({secret: 'secret'}));
+    app.use(express.session({secret: 'secret'},new MongoStore({ db: 'mongodb://heroku:1d5c275dc705b0eb1e28ae318faacfee@alex.mongohq.com:10097/app8947109' })));
     app.use(express.methodOverride());
     app.use(app.router);
 });
